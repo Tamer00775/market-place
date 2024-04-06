93afdd0a681117ff07a89020d7587454ccdeb38c
tamer00775 <tamerlankartaev04@gmail.com>
2024-04-06 14:25:28 +0500

    task-2 add enums for user and category

---
+++ src/main/java/kz/halyk/finservice/test/MarketPlace/enums/CategoryCode.java
@@ -0,0 +1,13 @@
+package kz.halyk.finservice.test.MarketPlace.enums;
+
+public enum CategoryCode {
+    FOOD,
+    CLOTHING,
+    ELECTRONICS,
+    COSMETICS,
+    HOME_GOODS,
+    SPORTS_AND_FITNESS,
+    BOOKS_AND_MAGAZINES,
+    FURNITURE;
+}
+
